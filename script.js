2e0deb6222cd9a2e133c4364c5ca6fb53c589f2e
Yulia Zulina <>
2025-05-22 12:29:14 +0300

    kek4

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
 let email_input = document.querySelector(".email_input");
 let btn_submit = document.querySelector(".btn-submit");
 let btn_return = document.querySelector(".btn-return");
-let remove_animation = function(){
-    this.classList.remove("animation");
-}
+let form = document.querySelector(".form");
 
 email_input.addEventListener("click", function(){
     if(this.value == "") this.value = "ash@loremcompany.com";
     this.classList.remove("error_input");
     document.querySelector(".email-error").classList.add("hidden");
-    document.querySelector(".btn-submit").classList.add("active-btn-submit");
 
 })
 email_input.addEventListener("input", function(){
     this.classList.remove("error_input");
     document.querySelector(".email-error").classList.add("hidden");
-    // document.querySelector(".btn-submit").classList.add("active-btn-submit");
 })
 
-email_input.addEventListener("animationend", remove_animation);
-btn_submit.addEventListener("animationend", remove_animation);
 btn_return.addEventListener("click", function(){
     document.querySelector(".main-content").classList.remove("hidden");
     document.querySelector(".succes-content").classList.add("hidden");
-    document.querySelector(".btn-submit").classList.remove("active-btn-submit");
 });
-
-document.addEventListener("click", function(e){
-    let email_input = document.querySelector(".email_input");
-    let btn_submit = document.querySelector(".btn-submit");
-    if(email_input.value == ""){
-        if(e.target != email_input && e.target != btn_submit){
-            email_input.classList.add("animation")
-        }
-    }
-    else{
-        if(e.target != btn_submit){
-            btn_submit.classList.add("animation")
-        }
-    }
-})
-
-let form = document.querySelector(".form");
 
 function validateEmail(email) {
     if (!email) return 0;
